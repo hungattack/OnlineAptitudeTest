95a59fc075ea9a034b95b57ee4ee822a711afbd6
hau.vv <hau.vv.2091@aptechlearning.edu.vn>
2023-09-21 10:28:38 +0700

    Create cateparts and question

---
+++ Controllers/CatePartsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using OnlineAptitudeTest.Model;
+using System.Collections;
+
+namespace OnlineAptitudeTest.Controllers
+{
+    [ApiController]
+    [Route("/Api/")]
+    public class CatePartsController : ControllerBase
+    {
+        private readonly AptitudeTestDbText db;
+        public CatePartsController(AptitudeTestDbText db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        [Route("[Controller]/[Action]")]
+        public IEnumerable<CateParts> List()
+        {
+            IEnumerable<CateParts> cateparts = db.CateParts.ToArray();
+            return cateparts;
+        }
+        [HttpGet]
+        [Route("[Controller]/[Action]/{id}")]
+        public IActionResult GetById(string id)
+        {
+            CateParts catepart = db.CateParts.Find(id);
+            if (catepart == null) { return NotFound(); }
+            return Ok(catepart);
+        }
+        [HttpGet]
+        [Route("[Controller]/[Action]/{name}")]
+        public Boolean Duppicated(string name)
+        {
+            Boolean flag = false;
+            CateParts catepart = db.CateParts.Where(cat => cat.Name.ToLower().Equals(name.ToLower())).FirstOrDefault();
+            if (catepart is not null) { flag = true; }
+            return flag;
+        }
+        [HttpPost]
+        [Route("[Controller]/[Action]")]
+        public IActionResult AddNew(CateParts catepart)
+        {
+            CateParts cateParts = db.CateParts.Find(catepart.Id);
+            if (cateParts is not null) { return NotFound("Catepart Id is exit"); };
+            if (!ModelState.IsValid)
+            {
+                return Ok(catepart.Name);
+            }
+            if (Duppicated(catepart.Name))
+            {
+                ModelState.AddModelError("DupplicateCatePartsName", "CateParts Name is dupplicate");
+                return Ok(ModelState);
+            }
+            db.Add(catepart);
+            db.SaveChanges();
+            return Ok(catepart);
+        }
+        [HttpDelete]
+        [Route("[Controller]/[Action]/{id}")]
+        public IActionResult Delete(string id)
+        {
+            CateParts cateParts = db.CateParts.Find(id);
+            if (cateParts is null) { return NotFound(); };
+            var question = db.Questions.Where(c => c.PartId == cateParts.Id).ToList();    
+            if(question is null) { return NotFound(); } 
+            foreach (var question1 in question)
+            {
+                db.Questions.Remove(question1);
+            }
+            db.CateParts.Remove(cateParts);
+            db.SaveChanges();
+            return Ok("Delete cpmplete");
+
+        }
+        [HttpPut]
+        [Route("[Controller]/[Action]")]
+        public IActionResult Update(CateParts catepart)
+        {
+            CateParts cateparts = db.CateParts.Find(catepart.Id);
+            if (cateparts is null)
+            {
+                return NotFound("Catepart not found");
+            }
+            cateparts.Name = catepart.Name;
+            cateparts.CreatedAt = DateTime.Now;
+
+            db.CateParts.Update(cateparts);
+            db.SaveChanges();
+            return Ok("Catepart update complete");
+        }
+    }
+}
---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,116 @@
+﻿using OnlineAptitudeTest.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Common;
+using System.Text.Json;
+using System.ComponentModel;
+
+namespace OnlineAptitudeTest.Controllers
+{
+    [ApiController]
+    [Route("/Api/")]
+   
+    public class QuestionController : ControllerBase
+    {
+        private readonly AptitudeTestDbText? db;
+        public QuestionController(AptitudeTestDbText db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        [Route("[Controller]/[Action]")]
+        public IEnumerable<Question> List()
+        {
+            IEnumerable<Question> questions = db.Questions.ToArray();
+            //if (!Question.Any()) {return NotFound();}
+            return questions;
+        }
+        [HttpGet]
+        [Route("[Controller]/[Action]/{id}")]
+        public IActionResult GetById(string id)
+        {
+            Question question = db.Questions.Find(id);
+            if (question is null) { return NotFound(); }
+            return Ok(question);
+        }
+        [HttpGet]
+        [Route("[Controller]/[Action]/{name}")]
+        public Boolean Dupplicated(string name)
+        {
+            Boolean flag = false;
+            Question question = db.Questions.Where(que => que.QuestionName.ToLower().Equals(name.ToLower())).FirstOrDefault();
+            if (question is not null) { flag = true; }
+            return flag;
+        }
+        [HttpGet]
+        [Route("[Controller]/[Action]/{id}")]
+        public Boolean ExitCatePart(string id)
+        {
+            Boolean flag = false;
+            CateParts _exitcate = db.CateParts.Where(exc => exc.Id.ToLower().Equals(id.ToLower())).FirstOrDefault();
+            if (_exitcate is not null) { flag = true; }
+            return flag;
+        }
+        [HttpPost]
+        [Route("[Controller]/[Action]")]
+        public IActionResult Addnew(Question question)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Ok(ModelState);
+            }
+            if (Dupplicated(question.QuestionName))
+            {
+                ModelState.AddModelError("DupplicateQuestionName", "Question Name is dupplicated!");
+                return Ok(ModelState);
+            }
+            if (!ExitCatePart(question.PartId))
+            {
+                return NotFound("Catepart is not exit");
+            }
+
+            db.Add(question);
+            db.SaveChanges();
+            return Ok(question);
+        }
+        [HttpDelete]
+        [Route("[Controller]/[Action]/{id}")]
+        public IActionResult Delete(string id)
+        {
+            Question question = db.Questions.Find(id);
+            if(question is null) { return NotFound(); };
+            db.Questions.Remove(question);  
+            db.SaveChanges();
+            return Ok("Delete Complete");
+
+        }
+        [HttpPut]
+        [Route("[Controller]/[Action]")]
+        public IActionResult Update(Question question)
+        {
+            Question _question = db.Questions.Find(question.Id);
+            
+            if (_question is null)
+            {
+                return NotFound("Question not found");
+            }
+            if (!ModelState.IsValid)
+            {
+                return Ok(ModelState);
+            }
+            if (!ExitCatePart(question.PartId))
+            {
+                return NotFound("Catepart is not exit");
+            }
+            _question.PartId = question.PartId;
+            _question.QuestionName = question.QuestionName;
+            _question.Answer = question.Answer;
+            _question.Point = question.Point;
+            _question.TimeOut = question.TimeOut;
+            _question.UpdatedAt = DateTime.Now;
+
+            db.Questions.Update(_question);
+            db.SaveChanges();
+            return Ok("Question update complete");
+        }
+    }
+}
